1103d35176e8bbf2e903257fc384305e831278f9
Om <90174038+omchaudhari1107@users.noreply.github.com>
2024-01-19 06:51:27 +0530

    Update urls.py

--- website/urls.py
+++ website/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import path
 from app.views import *
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('',home,name='home'),
     path('about/',about,name='about'),
     path('skills/',skills,name='skills'),
